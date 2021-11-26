3e6c25cae72e2727a67ce0e0fcd07a58dac2d78f
MaximZag <maximus9023@gmail.com>
2021-11-26 23:23:32 +0200

    1.6

--- lesson6-hw/additional/script.js
+++ lesson6-hw/additional/script.js
@@ -2,7 +2,7 @@
 // document.writeln(cutString('наслаждение',3)) // [нас,лаж,ден,ие]
 
 
-let resstr=[];
+let resstr = [];
 let cutString = (str, n) => {
     while (str.length) {
         resstr.push(str.substr(0, n))
@@ -11,3 +11,21 @@ let cutString = (str, n) => {
     console.log(resstr)
 };
 cutString('наслаждение', 4);
+
+// - Напишіть функцію count(str, stringsearch), яка повертає кількість символів stringsearch у рядку str.
+//     let symb = "о", str = "Астрономия это наука о небесных объектах";
+// document.writeln(count(str, symb)) // 5
+
+let n = 0;
+let k = 0;
+// debugger
+let find = (string, search) => {
+    while (string.length) {
+        n = string.indexOf(search);
+        k++;
+        string = string.slice(n+1);
+    }
+    console.log(n);
+    console.log(string);
+}
+find("Астрономия это наука о небесных объектах", 'о');
