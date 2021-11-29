0b75d8314e3a9ecd0cb6264fc97b7b7ce58546d2
MaximZag <maximus9023@gmail.com>
2021-11-29 11:05:43 +0200

    1.7

--- lesson7-hw/classwork/script.js
+++ lesson7-hw/classwork/script.js
@@ -93,6 +93,49 @@ car2.addDriver('Kolya');
 console.log(car2);
 
 // -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
+
+class Cinderella{
+    constructor(name,age,foot) {
+        this.name=name;
+        this.age=age;
+        this.foot=foot;
+    }
+}
+let cindarray=[];
+cindarray[0]=new Cinderella('Olya', 20,33);
+cindarray[1]=new Cinderella('Tanya', 19,32);
+cindarray[2]=new Cinderella('Katya', 22,30);
+cindarray[3]=new Cinderella('Yana', 23,36);
+cindarray[4]=new Cinderella('Anya', 21,35);
+cindarray[5]=new Cinderella('Ulya', 28,35);
+cindarray[6]=new Cinderella('Emma', 40,37);
+cindarray[7]=new Cinderella('Toma', 24,39);
+cindarray[8]=new Cinderella('Lena', 29,40);
+cindarray[9]=new Cinderella('Ira', 26,38);
+console.log(cindarray);
+
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
+
+class Prince{
+    constructor(name, age, shoes) {
+        this.name=name;
+        this.age=age;
+        this.shoes=shoes;
+    }
+}
+
+let prince=new Prince('Kolya', 33, 39);
+console.log(prince);
+
 //     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
+
+for (let i=0; i<cindarray.length; i++) {
+    if (cindarray[i].foot===prince.shoes){
+        console.log(`${prince.name} повинен бути з ${cindarray[i].name}`)
+    }
+}
+
 //     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
+
+let find=cindarray.find(value => value.foot===prince.shoes);
+console.log(`Принц твоя попелюшка ${find.name}`);
