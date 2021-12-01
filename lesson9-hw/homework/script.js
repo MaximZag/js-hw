d40d5ac55569cff35601053d429742e027dfe851
MaximZag <maximus9023@gmail.com>
2021-12-01 21:51:36 +0200

    1.9

--- lesson9-hw/homework/script.js
+++ lesson9-hw/homework/script.js
@@ -38,20 +38,24 @@ for (const item of array) {
     ul.appendChild(li);
 }
 
-
-//
 // - Є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 // Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
 // Завдання робити через цикли.
-//
+
+for (const coursesAndDurationArrayElement of coursesAndDurationArray) {
+    let coursesBlock = document.createElement('div');
+    coursesBlock.innerText = `Course ${coursesAndDurationArrayElement.title} with duration ${coursesAndDurationArrayElement.monthDuration}`;
+    document.body.appendChild(coursesBlock);
+}
+
 // - Є масив
 // let coursesAndDurationArray = [
 //     {title: 'JavaScript Complex', monthDuration: 5},
@@ -63,3 +67,16 @@ for (const item of array) {
 // ];
 // За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
 //     Завдання робити через цикли.
+
+for (const coursesAndDurationArrayElement of coursesAndDurationArray) {
+    let div = document.createElement('div');
+    div.classList.add('item');
+    let h1 = document.createElement('h1');
+    h1.classList.add('heading');
+    h1.innerText = coursesAndDurationArrayElement.title;
+    let p = document.createElement('p');
+    p.classList.add('description');
+    p.innerText = coursesAndDurationArrayElement.monthDuration;
+    document.body.appendChild(div);
+    div.append(h1, p);
+}
