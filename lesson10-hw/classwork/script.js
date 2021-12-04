aed073c6e04b5a76ee58065a99c46e68bf1292a8
MaximZag <maximus9023@gmail.com>
2021-12-04 16:03:40 +0200

    1.10

--- lesson10-hw/classwork/script.js
+++ lesson10-hw/classwork/script.js
@@ -1,7 +1,34 @@
 // - створити 2 форми  по 2 інпути в кожній. ствоирити кнопку при кліку на яку считується та виводиться на консоль інформація з цих 2х форм.
 //     Кнопка повинна лежати за межами форм (Щоб ьуникнути  перезавантаження сторінки)
 // Доступ до інпутів через Forms API. Отже дайте формі та інпутам всі необхідні атрибути.
-//
+
+let form1=document.createElement('form');
+form1.setAttribute('name','form1');
+let form2=document.createElement('form');
+form2.setAttribute('name','form2');
+let input1=document.createElement('input');
+input1.setAttribute('name','input1');
+let input2=document.createElement('input');
+input2.setAttribute('name','input2');
+let input3=document.createElement('input');
+input3.setAttribute('name','input3');
+let input4=document.createElement('input');
+input4.setAttribute('name','input4');
+let button=document.createElement('button');
+button.id='but';
+button.innerText='Submit';
+
+document.body.append(form1,form2, button);
+form1.append(input1,input2);
+form2.append(input3,input4);
+
+button.onclick=function () {
+    console.log(input1.value +' - '+input2.value);
+    console.log(input2.value +' - '+input3.value);
+}
+
+
+
 //
 // - Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
 //     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
