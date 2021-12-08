ad9513831f4fe5275a21a3f940669ebc107fad28
MaximZag <maximus9023@gmail.com>
2021-12-08 23:32:04 +0200

    1.11

--- lesson11-hw/classwork/script.js
+++ lesson11-hw/classwork/script.js
@@ -14,3 +14,25 @@ let users = [
 ];
 // створити під кожен об'єкт свій блок з конопкою "додати до улюблених" при натисканні на яку об'єкт потрапляє до масиву favorites улюблених обраних об'єктів в локальному сховищі.
 // Створити сторніку favorites.html при переході на яку потрібно вивест в документ всіх обраних на попередньому етапі.
+let i = 0;
+let contain = document.createElement('div');
+document.body.appendChild(contain);
+contain.style.display = 'flex';
+contain.style.flexWrap = 'wrap';
+for (const user of users) {
+    let div = document.createElement('div');
+    let button = document.createElement('button');
+    button.innerText = 'Add to favorites';
+    button.style.marginTop = '10px';
+    div.style.width = '60%';
+    div.style.fontSize = '20px';
+    contain.append(div, button);
+    div.style.marginTop = '10px';
+    div.innerText = user.name + ' ' + user.age + 'age ' + user.status;
+    button.onclick = function () {
+        i++;
+        localStorage.setItem(`${i}`, JSON.stringify(user));
+        button.style.display = 'none';
+        div.style.color = 'red';
+    }
+}
--- lesson11-hw/homework/script.js
+++ lesson11-hw/homework/script.js
@@ -1,23 +1,35 @@
 // -створити форму з інпутами для name та age.
 //     При відправці форми записати об'єкт в localstorage
 
-let form=document.createElement('form');
-form.innerText='Enter name and age';
-let inputName=document.createElement('input');
-let inputAge=document.createElement('input');
-let button=document.createElement('button');
-button.innerText='Submit';
+let form = document.createElement('form');
+form.innerText = 'Enter name and age';
+let inputName = document.createElement('input');
+let inputAge = document.createElement('input');
+let button = document.createElement('button');
+button.innerText = 'Submit';
 document.body.append(form, button);
 form.append(inputName, inputAge);
 
-button.onclick=function () {
-console.log(inputName.value);
+button.onclick = function () {
+    console.log(inputName.value);
     localStorage.setItem('name', inputName.value);
     localStorage.setItem('age', inputAge.value);
 }
 
 
-
-
 // -створити форму з інпутами для model,type та volume автівки.
 //     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+
+let formCar = document.createElement('form');
+formCar.innerText = 'Enter model, type and volume of the car';
+let inputModel = document.createElement('input');
+let inputType = document.createElement('input');
+let inputVolume = document.createElement('input');
+let button2 = document.createElement('button');
+button2.innerText = 'Submit';
+document.body.append(formCar, button2);
+formCar.append(inputModel, inputType, inputVolume);
+
+button2.onclick = function () {
+    localStorage.setItem('car', JSON.stringify([{'Model': inputModel.value}, {'Type': inputType.value}, {'Volume': inputVolume.value}]));
+}
