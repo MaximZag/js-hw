555410ff1a5aab5ea7e9e10e70e329d156abc328
MaximZag <maximus9023@gmail.com>
2021-12-09 00:49:51 +0200

    1.11

--- lesson11-hw/classwork/favoritesscript.js
+++ lesson11-hw/classwork/favoritesscript.js
@@ -1,11 +1,11 @@
 let array = Object.keys(localStorage);
-array.sort((a, b) => a - b);
 document.write('Favorites:');
 for (const item of array) {
     let div = document.createElement('div');
     document.body.appendChild(div);
     div.style.marginTop = '10px';
     let parse = JSON.parse(localStorage.getItem(item));
-    div.innerText = item + ' - ' + parse.name + ' ' + parse.age + ' ' + parse.status;
+    div.innerText = parse.name + ' ' + parse.age + ' ' + parse.status;
+    div.style.fontSize = '20px';
 }
 
--- lesson11-hw/classwork/script.js
+++ lesson11-hw/classwork/script.js
@@ -9,12 +9,13 @@ let users = [
     {name: 'oleg', age: 28, status: false},
     {name: 'andrey', age: 29, status: true},
     {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
+    {name: 'ulya', age: 31, status: false},
+    {name: 'den', age: 31, status: true}
 ];
 // створити під кожен об'єкт свій блок з конопкою "додати до улюблених" при натисканні на яку об'єкт потрапляє до масиву favorites улюблених обраних об'єктів в локальному сховищі.
 // Створити сторніку favorites.html при переході на яку потрібно вивест в документ всіх обраних на попередньому етапі.
 let i = 0;
+// let array = [];
 let contain = document.createElement('div');
 document.body.appendChild(contain);
 contain.style.display = 'flex';
@@ -22,17 +23,34 @@ contain.style.flexWrap = 'wrap';
 for (const user of users) {
     let div = document.createElement('div');
     let button = document.createElement('button');
+    let removebutton = document.createElement('button');
     button.innerText = 'Add to favorites';
+    removebutton.innerText = 'Remove from favorites';
     button.style.marginTop = '10px';
+    removebutton.style.marginTop = '10px';
+    removebutton.style.display = 'none';
     div.style.width = '60%';
     div.style.fontSize = '20px';
-    contain.append(div, button);
+    contain.append(div, button, removebutton);
     div.style.marginTop = '10px';
     div.innerText = user.name + ' ' + user.age + 'age ' + user.status;
     button.onclick = function () {
         i++;
-        localStorage.setItem(`${i}`, JSON.stringify(user));
+        // localStorage.setItem(`${i}`, JSON.stringify(user));
+        localStorage.setItem(user.name, JSON.stringify(user));
         button.style.display = 'none';
+        removebutton.style.display = 'block';
         div.style.color = 'red';
+    }
+    removebutton.onclick = function () {
+        let array2 = Object.keys(localStorage);
+        for (const item of array2) {
+            if (JSON.parse(localStorage.getItem(item)).name === user.name) {
+                localStorage.removeItem(item);
+                removebutton.style.display = 'none';
+                button.style.display = 'block';
+                div.style.color = 'black';
+            }
+        }
     }
 }
