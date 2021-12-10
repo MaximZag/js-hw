70a02c86b2fa9442b654f62b5a4ff2076fcaf227
MaximZag <maximus9023@gmail.com>
2021-12-11 01:00:07 +0200

    1.12

--- lesson12-hw/additional/script.js
+++ lesson12-hw/additional/script.js
@@ -6,25 +6,41 @@
 
 let cont = document.createElement('div');
 document.body.append(cont);
-
+cont.style.display='flex';
+cont.style.flexWrap='wrap';
+cont.style.columnGap='3px';
+cont.style.rowGap='3px';
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(value => value.json())
     .then(users => {
         for (const user of users) {
             let div = document.createElement('div');
+            div.style.width='24%';
+            div.style.border='1px solid black';
+            div.style.borderRadius='5px';
+            div.style.display='flex';
+            div.style.flexDirection='column';
+            div.style.alignItems='center';
             cont.appendChild(div);
             let h2 = document.createElement('h2');
             h2.innerText = `${user.name}`;
             let button = document.createElement('button');
+            let buttonHide = document.createElement('button');
             button.innerText = 'Show';
-            div.append(h2, button);
+            buttonHide.innerText = 'Hide';
+            buttonHide.style.display='none';
+            button.style.marginBottom='10px';
+            div.append(h2, button,buttonHide);
             button.onclick = function () {
+                button.style.display='none';
+                buttonHide.style.display='block';
                 Object.entries(user).forEach(([key, value]) => {
                     if (typeof value === 'object') {
                         let div2 = document.createElement('div');
                         div2.innerText = `${key}`;
                         div2.style.border='1px solid green';
                         div2.style.borderRadius='5px';
+                        div2.style.padding='10px';
                         let button2 = document.createElement('button');
                         button2.innerText = 'Show';
                         div.appendChild(div2);
@@ -35,6 +51,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
                                         let div4=document.createElement('div');
                                         div4.innerText=`${key1}`;
                                         div4.style.border='1px solid green';
+                                        div4.style.padding='10px';
                                         div4.style.borderRadius='5px';
                                         let button3 = document.createElement('button');
                                         button3.innerText = 'Show';
@@ -50,6 +67,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
                                     }else {
                                         let div3 = document.createElement('div');
                                         div3.innerText = `${key1}: ${value1}`;
+                                        div3.style.padding='10px';
                                         div2.appendChild(div3);
                                     }
                                 });
@@ -60,11 +78,16 @@ fetch('https://jsonplaceholder.typicode.com/users')
                         userDiv.innerText = `${key}: ${value}`;
                         userDiv.style.border = '1px solid green';
                         userDiv.style.textAlign = 'center';
+                        userDiv.style.padding='10px';
                         userDiv.style.margin = '2px';
                         userDiv.style.borderRadius = '5px';
                         div.appendChild(userDiv);
                     }
                 });
+            }
+            buttonHide.onclick=function () {
+                div2.style.display='none';
+                alert('123');
             }
 
 
