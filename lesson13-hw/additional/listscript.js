51470d08a1f8d429a53088086423a5f5857b6dad
MaximZag <maximus9023@gmail.com>
2021-12-14 22:22:08 +0200

    1.13

--- lesson13-hw/additional/listscript.js
+++ lesson13-hw/additional/listscript.js
@@ -1,8 +1,18 @@
 let array = Object.keys(localStorage);
-console.log(array);
-document.write('Products:');
+    let buttonClear=document.createElement('button');
+    buttonClear.innerText='Delete all products';
+    document.body.appendChild(buttonClear);
+    buttonClear.style.width='50%';
+    buttonClear.style.fontSize='20px';
+    buttonClear.style.display='none';
     let div = document.createElement('div');
+    div.style.display='flex';
+    div.style.flexWrap='wrap';
     document.body.appendChild(div);
+    if (array.length){
+        buttonClear.style.display='block';
+    }
+
 for (const item of array) {
     let parse = JSON.parse(localStorage.getItem(item));
     let itemdiv=document.createElement('div');
@@ -10,14 +20,21 @@ for (const item of array) {
     let quantity=document.createElement('h2');
     let price=document.createElement('h2');
     let image=document.createElement('img');
+    itemdiv.style.width='30%';
+    image.style.width='300px';
     div.appendChild(itemdiv);
     itemdiv.append(name, quantity, price,image);
-    // div.style.marginTop = '10px';
-    console.log(parse);
-    console.log(parse.quantity);
     name.innerText = item;
-    quantity.innerText= parse.quantity;
-    price.innerText= parse.price;
+    quantity.innerText= 'Qty - '+ parse.quantity;
+    price.innerText= 'Price: '+ parse.price;
     image.src=parse.image;
 
+}
+
+buttonClear.onclick=function () {
+    let array2=Object.keys(localStorage);
+    for (const item of array2) {
+        localStorage.removeItem(item);
+    }
+    document.location.reload();
 }
