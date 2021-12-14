3fbe97448d7c4a1d71d661936504d7616e018a5e
MaximZag <maximus9023@gmail.com>
2021-12-15 00:13:29 +0200

    1.13

--- lesson13-hw/additional/listscript.js
+++ lesson13-hw/additional/listscript.js
@@ -1,38 +1,77 @@
 let array = Object.keys(localStorage);
-    let buttonClear=document.createElement('button');
-    buttonClear.innerText='Delete all products';
-    document.body.appendChild(buttonClear);
-    buttonClear.style.width='50%';
-    buttonClear.style.fontSize='20px';
-    buttonClear.style.display='none';
-    let div = document.createElement('div');
-    div.style.display='flex';
-    div.style.flexWrap='wrap';
-    document.body.appendChild(div);
-    if (array.length){
-        buttonClear.style.display='block';
-    }
+let buttonClear = document.createElement('button');
+let buttonReturn = document.createElement('button');
+buttonClear.innerText = 'Delete all products';
+buttonReturn.innerText = 'Return to adding products';
+document.body.append(buttonReturn, buttonClear);
+buttonClear.style.width = '50%';
+buttonReturn.style.width = '50%';
+buttonClear.style.fontSize = '20px';
+buttonReturn.style.fontSize = '20px';
+buttonClear.style.display = 'none';
+buttonReturn.style.width = '40%';
+buttonClear.style.width = '40%';
+buttonClear.style.marginLeft = '5%';
+buttonReturn.style.marginLeft = '5%';
+
+if (array.length) {
+    buttonClear.style.display = 'inline';
+} else {
+    buttonReturn.style.width = '90%';
+}
+
+buttonReturn.onclick = function () {
+    document.location = 'index.html';
+}
+
+let div = document.createElement('div');
+div.style.display = 'flex';
+div.style.flexWrap = 'wrap';
+div.style.columnGap = '10px';
+div.style.rowGap = '10px';
+div.style.marginTop = '20px';
+document.body.appendChild(div);
 
 for (const item of array) {
     let parse = JSON.parse(localStorage.getItem(item));
-    let itemdiv=document.createElement('div');
-    let name=document.createElement('h2');
-    let quantity=document.createElement('h2');
-    let price=document.createElement('h2');
-    let image=document.createElement('img');
-    itemdiv.style.width='30%';
-    image.style.width='300px';
+    let itemdiv = document.createElement('div');
+    let name = document.createElement('h2');
+    let quantity = document.createElement('h2');
+    let price = document.createElement('h2');
+    let image = document.createElement('img');
+    let delButton = document.createElement('button');
+    delButton.innerText = 'Delete product';
+    itemdiv.style.width = '30%';
+    itemdiv.style.border = '1px solid black';
+    itemdiv.style.borderRadius = '5px';
+    itemdiv.style.display = 'flex';
+    itemdiv.style.flexDirection = 'column';
+    itemdiv.style.alignItems = 'center';
+    image.style.width = '90%';
+    image.style.height = '200px';
     div.appendChild(itemdiv);
-    itemdiv.append(name, quantity, price,image);
+    itemdiv.append(name, quantity, price, image, delButton);
     name.innerText = item;
-    quantity.innerText= 'Qty - '+ parse.quantity;
-    price.innerText= 'Price: '+ parse.price;
-    image.src=parse.image;
-
+    name.style.margin = '5px';
+    quantity.style.margin = '5px';
+    price.style.margin = '5px';
+    quantity.innerText = 'Qty - ' + parse.quantity;
+    price.innerText = 'Price: ' + parse.price;
+    image.src = parse.image;
+    delButton.style.margin = '5px';
+    delButton.onclick = function () {
+        localStorage.removeItem(item);
+        itemdiv.style.display = 'none';
+        let array3 = Object.keys(localStorage);
+        if (!array3.length) {
+            buttonClear.style.display = 'none';
+            buttonReturn.style.width = '90%';
+        }
+    }
 }
 
-buttonClear.onclick=function () {
-    let array2=Object.keys(localStorage);
+buttonClear.onclick = function () {
+    let array2 = Object.keys(localStorage);
     for (const item of array2) {
         localStorage.removeItem(item);
     }
--- lesson13-hw/additional/script.js
+++ lesson13-hw/additional/script.js
@@ -9,24 +9,71 @@
 // На сторінці  list.html побудувати кнопку яка видаляє всі товари з корзини та локалстораджа.
 //     До кожного товару додати кнопку, при кліку на яку з лс видаляється конкретний обраний  товар
 
-let form = document.createElement('form');
+// let form = document.createElement('form');
+let namediv = document.createElement('div');
+let quandiv = document.createElement('div');
+let pricediv = document.createElement('div');
+let imagediv = document.createElement('div');
 let inputName = document.createElement('input');
 let inputQuantity = document.createElement('input');
 let inputPrice = document.createElement('input');
 let inputImage = document.createElement('input');
 let button = document.createElement('button');
-let link=document.createElement('a');
-link.setAttribute('href', 'list.html');
-link.target= '_blank';
-link.innerText='На сторінку товарів';
+let link = document.createElement('button');
+let h2name = document.createElement('h2');
+let h2quantity = document.createElement('h2');
+let h2price = document.createElement('h2');
+let h2image = document.createElement('h2');
+link.innerText = 'Proceed to catalog';
+link.style.fontSize = '20px';
+button.style.fontSize = '20px';
 button.innerText = 'Submit';
-document.body.append(form, button, link);
-form.append(inputName, inputQuantity, inputPrice, inputImage);
+button.style.display = 'block';
+h2name.innerText = 'Enter Name';
+inputName.style.fontSize = '20px';
+inputQuantity.style.fontSize = '20px';
+inputPrice.style.fontSize = '20px';
+inputImage.style.fontSize = '20px';
+// inputImage.style.width='50%';
+h2quantity.innerText = 'Enter Quantity';
+h2price.innerText = 'Enter Price';
+h2image.innerText = 'Enter link to image';
+h2name.style.textAlign = 'center';
+h2quantity.style.textAlign = 'center';
+h2price.style.textAlign = 'center';
+h2image.style.textAlign = 'center';
+button.style.marginTop = '20px';
+link.style.marginTop = '100px';
+link.style.width = '50%';
+imagediv.style.width = '70%';
+inputImage.style.width = '100%';
+document.body.style.display = 'flex';
+document.body.style.flexDirection = 'column';
+document.body.style.alignItems = 'center';
+
+
+document.body.append(namediv, quandiv, pricediv, imagediv, button, link);
+namediv.append(h2name, inputName);
+quandiv.append(h2quantity, inputQuantity);
+pricediv.append(h2price, inputPrice);
+imagediv.append(h2image, inputImage);
+
+link.onclick = function () {
+    document.location = 'list.html';
+}
 
 button.onclick = function () {
-    localStorage.setItem(`${inputName.value}`, JSON.stringify({'quantity': inputQuantity.value, 'price': inputPrice.value, 'image': inputImage.value}));
-    inputName.value='';
-    inputQuantity.value='';
-    inputPrice.value='';
-    inputImage.value='';
+    if (inputName.value === '' || inputQuantity.value === '' || inputPrice.value === '' || inputImage.value === '') {
+        alert('Enter values in all fields before submitting');
+    } else {
+        localStorage.setItem(`${inputName.value}`, JSON.stringify({
+            'quantity': inputQuantity.value,
+            'price': inputPrice.value,
+            'image': inputImage.value
+        }));
+        inputName.value = '';
+        inputQuantity.value = '';
+        inputPrice.value = '';
+        inputImage.value = '';
+    }
 }
